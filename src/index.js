7df4b87ca81934c53cf839888d8c1a42da99bd98
JerryZhu99 <jerryzhu99@gmail.com>
2021-07-15 03:20:11 +0000

    Deploying to gh-pages from @ JerryZhu99/mania-diffcalc@19280ac9077d616c707adfed3e06d2dbd03cd69d 🚀

---
+++ src/index.js
@@ -0,0 +1,240 @@
+
+
+const reworkDifficulty = require('./rework_diffcalc');
+const stableDifficulty = require('./stable_diffcalc');
+const { parseBeatmap, getTimingWindow, formatMetadata } = require('./utils');
+
+(async () => {
+  const dataSets = [
+    'reform-dans',
+    'ln-v2-dans',
+    '7k-regular-dans',
+    '7k-ln-dans',
+    'ranked-4k',
+    'ranked-7k',
+  ]
+
+  let mapData = await Promise.all(dataSets.map(async e => [await (await fetch(`/output/${e}.json`)).json(), e]));
+  mapData.push([[], 'custom'])
+
+  const toPlotData = (data, name) => ({
+    x: data.map(e => e.oldRating),
+    y: data.map(e => e.newRating),
+    mode: 'markers',
+    type: 'scattergl',
+    name: name,
+    text: data.map(e => formatMetadata(e.metadata)),
+    marker: {
+      size: 6,
+      opacity: 0.5,
+    },
+  })
+
+  const plotData = mapData.map(e => toPlotData(...e));
+
+  const layout = {
+    title: 'osu! reworked SR',
+    xaxis: {
+      title: 'Old SR',
+      rangemode: 'nonnegative',
+      constrain: 'range',
+      dtick: 1,
+    },
+    yaxis: {
+      title: 'New SR',
+      scaleanchor: 'x',
+      scaleratio: 1,
+      dtick: 1,
+    },
+    height: 800,
+    hovermode: 'closest',
+    shapes: [
+      {
+        type: 'line',
+        x0: 0,
+        y0: 0,
+        x1: 10,
+        y1: 10,
+        line: {
+          color: 'rgb(55, 128, 191)',
+          width: 3
+        },
+      },
+    ]
+  };
+
+  Plotly.react('difficulty-scatter', plotData, layout, { displaylogo: false });
+
+  const toTableData = (data, name) => data.map(e => ({
+    name: formatMetadata(e.metadata),
+    group: name,
+    ratingChange: e.newRating - e.oldRating,
+    ...e,
+  }))
+
+  const tableData = [].concat(...mapData.map(e => toTableData(...e)));
+  const ratingFormatter = e => e?.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+  const durationFormatter = duration => {
+    const pad = n => ('00' + n).slice(-2);
+    const ms = duration % 1000;
+    duration = (duration - ms) / 1000;
+    const secs = duration % 60;
+    duration = (duration - secs) / 60;
+    const mins = duration % 60;
+    const hrs = (duration - mins) / 60;
+    return hrs > 0 ? `${hrs}:${pad(mins)}:${pad(secs)}` : `${mins}:${pad(secs)}`;
+  }
+
+  const difficultyTable = $('#difficulty-table');
+  let highlightRows = false;
+
+  difficultyTable.bootstrapTable({
+    data: tableData,
+    search: true,
+    searchTimeOut: 0,
+    showFullscreen: "true",
+    showColumns: "true",
+    buttons: () => ({
+      btnAdd: {
+        text: 'Highlight changes',
+        icon: 'fa-palette',
+        event: () => {
+          highlightRows = !highlightRows;
+          difficultyTable.bootstrapTable('refreshOptions', {
+            rowStyle: (row, index) => {
+              if (!highlightRows) return {};
+              let classes = '';
+              if (row.ratingChange > 1.5) classes = 'table-success';
+              if (row.ratingChange < -1.5) classes = 'table-danger';
+              return { classes };
+            },
+          })
+        },
+        attributes: {
+          title: 'Show star rating changes with colour',
+        },
+      },
+    }),
+    columns: [{
+      field: 'metadata',
+      title: 'Map',
+      sortable: true,
+      formatter: formatMetadata,
+    }, {
+      field: 'columnCount',
+      title: 'Keys',
+      sortable: true,
+    }, {
+      field: 'length',
+      title: 'Length',
+      sortable: true,
+      align: 'right',
+      halign: 'right',
+      formatter: durationFormatter,
+    }, {
+      field: 'lnPercent',
+      title: 'LN %',
+      sortable: true,
+      formatter: e => `${(e * 100)?.toFixed(0)}%`,
+    }, {
+      field: 'oldRating',
+      title: 'Old SR',
+      sortable: true,
+      formatter: ratingFormatter,
+    }, {
+      field: 'newRating',
+      title: 'New SR',
+      sortable: true,
+      formatter: ratingFormatter,
+    }, {
+      field: 'ratingChange',
+      title: 'SR Change',
+      sortable: true,
+      formatter: ratingFormatter,
+    }, {
+      field: 'group',
+      title: 'Group',
+      sortable: true,
+      visible: false,
+    }],
+  })
+
+
+  const plotStrains = (name, notes) => {
+    const getPlotData = (name, visible = false) => ({
+      x: notes.map(e => new Date(e.time)),
+      y: notes.map(e => e[name]),
+      mode: 'markers',
+      type: 'scattergl',
+      name: name,
+      text: notes.map(e => e.time + "|" + e.column),
+      marker: {
+        size: 4,
+        opacity: 0.5,
+      },
+      visible: visible || "legendonly",
+    })
+
+    const plotData = [
+      getPlotData('strain', true),
+      getPlotData('maxColumn'),
+      getPlotData('devColumn'),
+      getPlotData('baseStrain'),
+      getPlotData('longNoteBonus'),
+      getPlotData('staminaBonus'),
+      getPlotData('releaseCoverage'),
+    ]
+
+    const layout = {
+      title: name,
+      xaxis: {
+        title: 'Time',
+        rangemode: 'nonnegative',
+        constrain: 'range',
+        tickformat: '%M:%S'
+      },
+      yaxis: {
+        title: 'Strain',
+        dtick: 1,
+      },
+      height: 400,
+      hovermode: 'closest',
+    };
+
+    Plotly.react('difficulty-graph', plotData, layout, { displaylogo: false });
+  }
+
+  const osuUpload = document.getElementById('osu-upload');
+  const difficultyInfo = document.getElementById('difficulty-info');
+
+  osuUpload.addEventListener('change', () => {
+    const file = osuUpload.files[0];
+    const fr = new FileReader();
+    fr.addEventListener('load', () => {
+      const rawData = fr.result;
+      const map = parseBeatmap(rawData);
+      const oldRating = stableDifficulty.calculateDifficulty(map.columnCount, map.notes);
+      const newRating = reworkDifficulty.calculateDifficulty(map.columnCount, map.notes, getTimingWindow(map.columnCount));
+      const metadata = formatMetadata(map.metadata);
+      const customPlot = plotData.find(e => e.name == 'custom');
+
+      customPlot.x.push(oldRating);
+      customPlot.y.push(newRating);
+      customPlot.text.push(metadata);
+
+      Plotly.redraw('difficulty-scatter');
+
+      difficultyTable.bootstrapTable('append', toTableData([{ ...map, oldRating, newRating }], 'custom'))
+
+      plotStrains(metadata, map.notes)
+
+
+      difficultyInfo.innerText = `New SR: ${newRating.toFixed(2)}
+      Old SR: ${oldRating.toFixed(2)}
+      Diff: ${(newRating - oldRating).toFixed(2)}`;
+    });
+    fr.readAsText(file);
+  });
+
+})();
---
+++ src/rework_diffcalc.js
@@ -0,0 +1,176 @@
+const { getTimingWindow } = require("./utils");
+
+const parameters = {
+  STRAIN_FACTOR: 1.6,
+  STAMINA_HALF_LIFE: 4000,
+  STAMINA_STRAIN_FACTOR: 0.5,
+
+  LN_SHORT_BONUS: 0.2,
+  LN_SHORT_THRESHOLD: 200,
+  LN_LONG_THRESHOLD: 500,
+  LN_LONG_BONUS: 0.2,
+
+  LN_INVERSE_MIN_THRESHOLD: 200,
+  LN_INVERSE_BONUS: 0.6,
+
+  LN_RELEASE_MIN_THRESHOLD: 400,
+  LN_RELEASE_MAX_THRESHOLD: 600,
+  LN_RELEASE_BONUS: 1,
+
+  NEIGHBOURHOOD_SIZE: 400, //ms
+  DEVIATION_WEIGHT: 0.75,
+};
+
+function preprocessNotes(columns, notes) {
+  // Per finger
+  for (let i = 0; i < columns; i++) {
+    let columnNotes = notes.filter(e => e.column == i);
+    for (let j = 1; j < columnNotes.length; j++) {
+      columnNotes[j].columnPrev = columnNotes[j - 1];
+      columnNotes[j].columnDelta = columnNotes[j].time - columnNotes[j - 1].time;
+      columnNotes[j - 1].columnNext = columnNotes[j];
+    }
+  }
+
+  // Releases with significant overlap with held LN
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].releaseCoverage = 0;
+  }
+
+  for (let i = 0; i < notes.length; i++) {
+    for (let j = i + 1; j < notes.length; j++) {
+      if (notes[j].time > notes[i].endTime) {
+        break;
+      }
+      if (notes[j].endTime < notes[i].endTime) {
+        const start = Math.max(notes[j].endTime - parameters.LN_RELEASE_MAX_THRESHOLD / 2, notes[i].time)
+        const end = Math.min(notes[j].endTime + parameters.LN_RELEASE_MAX_THRESHOLD / 2, notes[i].endTime)
+        const range = Math.max(0, end - start);
+        const coverage = (range - parameters.LN_RELEASE_MIN_THRESHOLD)
+          / (parameters.LN_RELEASE_MAX_THRESHOLD - parameters.LN_RELEASE_MIN_THRESHOLD);
+
+        notes[j].releaseCoverage = Math.max(notes[j].releaseCoverage, coverage);
+      }
+    }
+  }
+
+  // Neighbourhood
+  let startIndex = 0;
+  for (let i = 0; i < notes.length; i++) {
+    // notes[i].neighbours = notes.filter(note => Math.abs(note.time - notes[i].time) < parameters.NEIGHBOURHOOD_SIZE);
+    // Optimization of the above
+    notes[i].neighbours = []
+    for (let j = startIndex; j < notes.length; j++) {
+      if (notes[j].time < notes[i].time - parameters.NEIGHBOURHOOD_SIZE) {
+        startIndex = j + 1;
+      } else if (notes[j].time > notes[i].time + parameters.NEIGHBOURHOOD_SIZE) {
+        break;
+      } else {
+        notes[i].neighbours.push(notes[j])
+      }
+    }
+  }
+}
+
+function calculateStrains(columns, notes, timingWindow) {
+  if (notes.length == 0) return 0;
+
+  preprocessNotes(columns, notes);
+
+  for (let i = 0; i < notes.length; i++) {
+    let currentNote = notes[i];
+
+    let {
+      columnPrev: previousNote,
+      columnNext: nextNote,
+    } = currentNote;
+
+    // Base strain
+    let columnDensities = []
+    for (let j = 0; j < columns; j++) {
+      let notes = currentNote.neighbours.filter(e => e.column == j);
+      columnDensities[j] = notes
+        .map(note => {
+          let delta = Math.abs(note.time - currentNote.time);
+          let weight = (parameters.NEIGHBOURHOOD_SIZE - delta) / parameters.NEIGHBOURHOOD_SIZE
+          return parameters.STRAIN_FACTOR * Math.sqrt(weight);
+        })
+        .reduce((a, b) => a + b, 0);
+    }
+
+    const max = Math.max(...columnDensities)
+    const deviation = Math.sqrt(columnDensities.map(e => e * (max - e)).reduce((a, b) => a + b, 0));
+    let strain = max * (1 - parameters.DEVIATION_WEIGHT) + deviation * parameters.DEVIATION_WEIGHT;
+
+    currentNote.maxColumn = max;
+    currentNote.devColumn = deviation;
+    currentNote.baseStrain = strain;
+
+    // Burst stamina
+    currentNote.staminaBonus = 0
+    if (previousNote && previousNote.baseStrain) {
+      const timeDiff = currentNote.time - previousNote.time;
+      const currentStaminaStrain = (2 ** (-timeDiff / parameters.STAMINA_HALF_LIFE)) * previousNote.baseStrain;
+      currentNote.staminaBonus += currentStaminaStrain * parameters.STAMINA_STRAIN_FACTOR;
+    }
+
+    currentNote.baseStrain += currentNote.staminaBonus
+
+    // LN bonuses
+    let longNoteFactor = 0;
+    if (currentNote.isLN) {
+      let lnLength = currentNote.endTime - currentNote.time;
+      lnLength = Math.max(parameters.LN_SHORT_THRESHOLD, lnLength);
+      lnLength = Math.min(parameters.LN_LONG_THRESHOLD, lnLength);
+      const relativeLength = (lnLength - parameters.LN_SHORT_THRESHOLD)
+        / (parameters.LN_LONG_THRESHOLD - parameters.LN_SHORT_THRESHOLD);
+
+      // Give a bonus depending on LN length
+      longNoteFactor = relativeLength * parameters.LN_LONG_BONUS + (1 - relativeLength) * parameters.LN_SHORT_BONUS;
+
+      // If the next note is close to the release (shields / inverse)
+      if (nextNote) {
+        const delta = nextNote.time - currentNote.endTime;
+        const inverseFactor = Math.max(0, 1 - delta / parameters.LN_INVERSE_MIN_THRESHOLD);
+        // Only apply inverse buff to long LNs
+        longNoteFactor += relativeLength * inverseFactor * parameters.LN_INVERSE_BONUS;
+      }
+
+      // If the LN released during another LN body
+      longNoteFactor *= 1 + currentNote.releaseCoverage * parameters.LN_RELEASE_BONUS
+    }
+
+    currentNote.longNoteBonus = longNoteFactor * currentNote.baseStrain;
+
+    // Sum
+    currentNote.strain = currentNote.baseStrain + currentNote.longNoteBonus;
+  }
+}
+
+function calculateDifficulty(columns, notes, timingWindow) {
+  if (notes.length == 0) return 0;
+
+  calculateStrains(columns, notes.filter(e => e.column < Math.floor(columns / 2)), timingWindow);
+  calculateStrains(columns, notes.filter(e => e.column >= Math.floor(columns / 2)), timingWindow);
+
+  // Weighted average
+  const averageStrain = (notes.map(e => e.strain ** 6).reduce((a, b) => a + b, 0) / notes.length) ** (1 / 6);
+
+  return averageStrain;
+}
+
+function calculate(map) {
+  const columns = map.columnCount;
+  const notes = map.notes;
+  const mirrorNotes = map.notes.map(e => ({ ...e, column: columns - 1 - e.column }));
+  const timingWindow = getTimingWindow(map.overallDifficulty);
+  // Assume mirror
+  return Math.min(
+    calculateDifficulty(columns, notes, timingWindow),
+    calculateDifficulty(columns, mirrorNotes, timingWindow));
+}
+
+module.exports = {
+  calculateDifficulty,
+  calculate,
+}
